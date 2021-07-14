1ae926eef03569a6e5e52e86c08823f5ec01875b
--replace-all <mubasharmayo46@gmail.com>
2021-07-14 22:03:46 +0500

    firebase setting add

--- src/main.js
+++ src/main.js
@@ -5,7 +5,20 @@ import store from './store'
 import { BootstrapVue } from 'bootstrap-vue'
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css'
 import '../node_modules/bootstrap-vue/dist/bootstrap-vue.min.css'
+import firebase from "firebase";
 
+const firebaseConfig = {
+  apiKey: "AIzaSyBO6mLxYysaxslbSDIk8_xwQgNFfqUYVk0",
+  authDomain: "vuefirebase-316f6.firebaseapp.com",
+  databaseURL: "https://vuefirebase-316f6.firebaseio.com",
+  projectId: "vuefirebase-316f6",
+  storageBucket: "vuefirebase-316f6.appspot.com",
+  messagingSenderId: "115774097554",
+  appId: "1:115774097554:web:53e443b1c284a8bce4fe4b"
+};
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
 
 Vue.config.productionTip = false
 Vue.use(BootstrapVue)
